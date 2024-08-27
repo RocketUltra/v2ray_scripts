67e2141ee6d541b6b65318687a3dd1e30061cc69
一刀 <yhonglai@163.com>
2024-08-27 17:20:47 +0800

    feat 去除切换ssh端口，验证小火箭测速问题

--- fm_custom.sh
+++ fm_custom.sh
@@ -17,23 +17,25 @@ update_ports() {
     # 启动防火墙服务
     sudo systemctl start firewalld
 
-    # 修改SSH端口为2222
-    sudo sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config
+    # 关闭22端口可能会导致小火箭测速失败
+    # # 修改SSH端口为2222
+    # sudo sed -i 's/^#Port 22/Port 2222/' /etc/ssh/sshd_config
 
-    # 重启SSH服务
-    sudo systemctl restart sshd
+    # # 重启SSH服务
+    # sudo systemctl restart sshd
 
-    # 检查22端口是否开启，如果开启，则禁用22端口
-    if sudo firewall-cmd --list-ports | grep -wq "22/tcp"; then
-        sudo firewall-cmd --zone=public --remove-port=22/tcp --permanent
-    fi
+    # # 检查22端口是否开启，如果开启，则禁用22端口
+    # if sudo firewall-cmd --list-ports | grep -wq "22/tcp"; then
+    #     sudo firewall-cmd --zone=public --remove-port=22/tcp --permanent
+    # fi
 
     # 开启v2ray端口
     sudo firewall-cmd --zone=public --add-port=31535/tcp --permanent
     sudo firewall-cmd --zone=public --add-port=31535/udp --permanent
 
     sudo firewall-cmd --reload
-    echo "防火墙已启动，SSH端口已修改为2222，22端口已禁用，31535已启用。"
+    # echo "防火墙已启动，SSH端口已修改为2222，22端口已禁用，31535已启用。"
+    echo "防火墙已启动，31535已启用。"
 }
 
 set_timezone
